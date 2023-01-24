7c0ebc1c2fa4153d530ccb414847fe9aef0d4cd1
Tom Montgomery <tom.b.montgomery@gmail.com>
2023-01-24 09:06:08 +0200

    Main

--- app.js
+++ app.js
@@ -1,7 +1,49 @@
-const menu = document.querySelector('#mobile-menu');
-const menuLinks = document.querySelector('.navbar__menu');
+wordDef = document.getElementById("word");
+const wordObj = {};
+let word = wordObj.input;
 
-menu.addEventListener('click', function() {
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
-});
+const options = {
+	method: "GET",
+	headers: {
+		"X-RapidAPI-Key": "ff3876428fmsh544ea95f5c15913p19f4a0jsnf3b743c34e17",
+		"X-RapidAPI-Host": "wordsapiv1.p.rapidapi.com",
+	},
+};
+
+function checkEnter(event) {
+	if (event.keyCode == 13) {
+		let word = document.getElementById("name-input").value;
+		wordObj.input = word;
+        console.log(wordObj.input);
+        getWord(wordObj.input);
+	}
+}
+
+function getWord(word) {
+	let api = `https://wordsapiv1.p.rapidapi.com/words/${word}/definitions`;
+
+	fetch(api, options)
+		.then(function (response) {
+			let data = response.json();
+			return data;
+		})
+		.then(function (data) {
+			let test1 = data.definitions[0].definition;
+			return test1;
+		})
+		.then(function (test1) {
+			wordObj.def = test1;
+		})
+		.then(function () {
+			changeHTML();
+		})
+		.catch(function (err) {
+			console.error(err);
+		});
+}
+
+function changeHTML() {
+	wordDef.innerHTML = `${wordObj.input}: ${wordObj.def}`;
+    document.getElementById("word").style.backgroundColor = "#498f61";
+}
+
